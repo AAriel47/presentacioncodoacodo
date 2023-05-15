8a12c6862ae527205b761d7ce041dc04c4e0015f
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2023-05-15 18:27:54 -0300

    presentacion de js

---
+++ js/ticket.js
@@ -0,0 +1,23 @@
+var form =document.getElementById("form");
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+    let nombre = document.getElementById("validationCustom01").value;
+    let apellido = document.getElementById("validationCustom02").value;
+    let correo = document.getElementById("validationCustom03").value;
+    let cantidad = document.getElementById("validationCustom04").value;
+    let categoria = document.getElementById("validationCustom05").value;
+    let importe=0;
+    switch(categoria){
+        case "Estudiante":
+            importe = (Number(cantidad)*200)-((Number(cantidad)*200)*0.8);
+            break;
+        case "Trainee":
+            importe = (Number(cantidad)*200)-((Number(cantidad)*200)*0.5);
+            break;
+        case "Junior":
+            importe = (Number(cantidad)*200)-((Number(cantidad)*200)*0.15);
+            break;
+    }
+    document.getElementById("importe").innerHTML=importe;
+
+})
