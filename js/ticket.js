be7455b8d4b3dc72c65a12e71cdc79cdb08eea8e
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2023-05-16 21:43:55 -0300

    presentar

--- js/ticket.js
+++ js/ticket.js
@@ -1,4 +1,8 @@
 var form =document.getElementById("form");
+
+form.addEventListener("reset",e=>{
+    document.getElementById("importe").innerHTML="";})
+
 form.addEventListener("submit", e=>{
     e.preventDefault();
     let nombre = document.getElementById("validationCustom01").value;
@@ -19,13 +23,84 @@ form.addEventListener("submit", e=>{
             break;
     }
     document.getElementById("importe").innerHTML=importe;
-
 })
 
-function mostrar(){
+function mostrar(para){
+    if (para == "ventas"){ 
+        var disertantes = document.getElementById("orador");
+        disertantes.classList.remove("active");
+       var confe = document.getElementById("conferen");
+       confe.classList.remove("active");
+       var sitio = document.getElementById("lugar");
+       sitio.classList.remove("active");
+       var trans = document.getElementById("convierte");
+       trans.classList.remove("active");
+    }
     document.getElementById("oculto").style.display = "block";
 }
 
-function ocultar(){
+function ocultar(para){
+    if (para == "orador"){ 
+        var disertantes = document.getElementById("orador");
+        disertantes.classList.add("active");
+       var confe = document.getElementById("conferen");
+       confe.classList.remove("active");
+       var sitio = document.getElementById("lugar");
+       sitio.classList.remove("active");
+       var trans = document.getElementById("convierte");
+       trans.classList.remove("active");
+       var tickets = document.getElementById("ventas");
+       tickets.classList.remove("active");
+    }
+
+    if (para == "conferen"){ 
+        var disertantes = document.getElementById("orador");
+        disertantes.classList.remove("active");
+       var confe = document.getElementById("conferen");
+       confe.classList.add("active");
+       var sitio = document.getElementById("lugar");
+       sitio.classList.remove("active");
+       var trans = document.getElementById("convierte");
+       trans.classList.remove("active");
+       var tickets = document.getElementById("ventas");
+       tickets.classList.remove("active");
+    }
+
+    if (para == "lugar"){ 
+        var disertantes = document.getElementById("orador");
+        disertantes.classList.remove("active");
+       var confe = document.getElementById("conferen");
+       confe.classList.remove("active");
+       var sitio = document.getElementById("lugar");
+       sitio.classList.add("active");
+       var trans = document.getElementById("convierte");
+       trans.classList.remove("active");
+       var tickets = document.getElementById("ventas");
+       tickets.classList.remove("active");
+    }
+
+    if (para == "convierte"){ 
+        var disertantes = document.getElementById("orador");
+        disertantes.classList.remove("active");
+       var confe = document.getElementById("conferen");
+       confe.classList.remove("active");
+       var sitio = document.getElementById("lugar");
+       sitio.classList.remove("active");
+       var trans = document.getElementById("convierte");
+       trans.classList.add("active");
+       var tickets = document.getElementById("ventas");
+       tickets.classList.remove("active");
+    }
+
     document.getElementById("oculto").style.display = "none";
 }
+
+function encuesta(){
+    var form2 = document.getElementById("form2");
+    form2.addEventListener("submit",e=>{
+        let nombre = document.getElementById("validationCustom01").value;
+        let apellido = document.getElementById("validationCustom02").value;
+        let descripcion = document.getElementById("validationCustom03").value;
+       // alert("datos cargados: "+" "+nombre+"  "+apellido+"  "+descripcion);
+    }
+)}
